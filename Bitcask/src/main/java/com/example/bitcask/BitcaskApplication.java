815e97eaa04eb983ffe324304d0b9c716fcac0b1
michaelmonir <michaael.monir119@gmail.com>
2024-04-28 06:21:54 +0300

    added the bitcask project

---
+++ Bitcask/src/main/java/com/example/bitcask/BitcaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitcask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcaskApplication.class, args);
+	}
+
+}
---
+++ Bitcask/src/test/java/com/example/bitcask/BitcaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitcask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
