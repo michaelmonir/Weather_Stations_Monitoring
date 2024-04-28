d6d7e6463ccfedfc7f4a9cb5ea00392eb5f92a10
michaelmonir <michaael.monir119@gmail.com>
2024-04-28 13:02:17 +0300

    added the write operation but without hashmap

--- Bitcask/src/main/java/com/example/bitcask/Bitcask/BitcaskWriter.java
+++ Bitcask/src/main/java/com/example/bitcask/Bitcask/BitcaskWriter.java
@@ -1,12 +1,20 @@
 package com.example.bitcask.Bitcask;
 
+import com.example.bitcask.File.FileOperations;
 import com.example.bitcask.Message.Message;
+import com.example.bitcask.Message.MessageToByteConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BitcaskWriter {
 
-    public void write(Message message) {
+    @Autowired
+    private FileOperations fileOperations;
 
+    public void write(Message message) {
+        MessageToByteConverter messageToByteConverter = new MessageToByteConverter(message);
+        byte[] data = messageToByteConverter.convert();
+        fileOperations.writeToFile(data);
     }
 }
