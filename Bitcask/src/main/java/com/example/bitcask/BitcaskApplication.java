ee52bc64c37274d886f1ed4d0cfceed124facbca
michaelmonir <michaael.monir119@gmail.com>
2024-04-30 14:07:03 +0300

    finished the scheduler but still testing the time

--- Bitcask/src/main/java/com/example/bitcask/BitcaskApplication.java
+++ Bitcask/src/main/java/com/example/bitcask/BitcaskApplication.java
@@ -1,11 +1,8 @@
 package com.example.bitcask;
 
 import com.example.bitcask.Bitcask.Bitcask;
-import com.example.bitcask.Compaction.CompactionStrategyImpl;
-import com.example.bitcask.Compaction.Compactor;
-import com.example.bitcask.Compaction.IntervalMerging.IntervalMergerStrategyImpl;
+import com.example.bitcask.Compaction.CompactionScheduler;
 import com.example.bitcask.Message.Message;
-import com.example.bitcask.Recovery.RecoveryInformationKeeper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,21 +12,17 @@ public class BitcaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BitcaskApplication.class, args);
 
+		Thread thread = new Thread(new CompactionScheduler());
+		thread.start();
+
 //		RecoveryInformationKeeper recoveryInformationKeeper = new RecoveryInformationKeeper();
 //		Bitcask.setBitcask(recoveryInformationKeeper.recover());
 
-		for (long i = 1; i <= 21; i++) {
+		for (long i = 1; i <= 10000000; i++) {
 			Message message = new Message(i, i, (short)i, i, (int)i, (int)i, (int)i);
 			Bitcask.getBitcask().write(message);
 		}
 
-		Compactor compactor = new Compactor(new CompactionStrategyImpl(), new IntervalMergerStrategyImpl());
-		compactor.compact();
-
-		Bitcask bitcask = Bitcask.getBitcask();
-
-		int z = 0;
-		z++;
 //		for (int i = 1; i <= 20; i++) {
 //			Message message = Bitcask.getBitcask().read((long)i);
 //			System.out.println(message.toString());
---
+++ Bitcask/src/main/java/com/example/bitcask/Compaction/CompactionScheduler.java
@@ -0,0 +1,24 @@
+package com.example.bitcask.Compaction;
+
+import com.example.bitcask.Bitcask.Bitcask;
+import com.example.bitcask.Compaction.IntervalMerging.IntervalMergerStrategyImpl;
+import com.example.bitcask.Exceptions.UnkownErrorException;
+
+public class CompactionScheduler implements Runnable {
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new UnkownErrorException();
+            }
+            Compactor compactor = new Compactor(new CompactionStrategyImpl(), new IntervalMergerStrategyImpl());
+            compactor.compact();
+            Bitcask bitcask = Bitcask.getBitcask();
+            int z = 0;
+            z++;
+        }
+    }
+}
