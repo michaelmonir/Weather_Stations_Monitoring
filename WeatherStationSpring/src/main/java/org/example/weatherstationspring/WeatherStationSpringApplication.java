02c4d585677352d93ec5966423e7550c89ac7bc1
ahmed elzainy <es-Ahmedkhalil2025@alexu.edu.eg>
2024-05-14 03:10:04 +0300

    added spring boot project

---
+++ WeatherStationSpring/src/main/java/org/example/weatherstationspring/WeatherStationSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.weatherstationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherStationSpringApplication.class, args);
+    }
+
+}
---
+++ WeatherStationSpring/src/test/java/org/example/weatherstationspring/WeatherStationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.weatherstationspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
